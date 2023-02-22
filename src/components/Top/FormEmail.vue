9d37ec7a19231986b03ce3653d6232fb8628a394
yU-kiki <yuki521ike@gmail.com>
2023-02-23 07:36:13 +0900

    メールアドレスの入力フォームを実装

--- src/components/Top/FormEmail.vue
+++ src/components/Top/FormEmail.vue
@@ -4,13 +4,19 @@
       <div class="textーmerit__wrapper">
         <p class="text-merit w6 fs20">rinneの最新情報を受け取る</p>
       </div>
-      <form class="form__wrapper mt16" action="" method="post">
+      <form name="gf_form" class="form__wrapper mt16" method="POST" target="hidden_iframe" :action="gf_doc" @submit.prevent="validationCheck">
         <div class="form-item__wrapper">
           <div class="form-item">
             <input
               class="input-email input"
-              value="example@rinne.help"
-              type="email"
+              :class="{ 'input-error': email_error }"
+              type="text"
+              area-label="email"
+              :id="'entry.' + survey[0].name"
+              :name="'entry.' + survey[0].name"
+              v-model="email"
+              :placeholder="survey[0].placeholder"
+              :disabled="submitted"
             />
           </div>
           <input
@@ -19,14 +25,58 @@
             type="submit"
           />
         </div>
+        <div v-show="email_error" class="error-msg__wrapper mt16">
+          <p class="error-msg">{{ email_error }}</p>
+        </div>
+        <div v-show="submitted" class="success-msg__wrapper mt16">
+          <p class="success-msg">メールアドレスを送信しました</p>
+        </div>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-
-
+export default {
+  name: 'FormEmail',
+  data() {
+    return {
+      email: '',
+      email_error: '',
+      submitted: false,
+      gf_doc: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSfTp3pDSzbpZ5u7UFx6o0ATw-soUb5vx9a6Vch7knTU_Ui8Lg/formResponse',
+      survey: [
+        {
+          name: '1537271753',
+          question: 'メールアドレス',
+          question_type: 'text',
+          label: 'email',
+          placeholder: 'example@rinne.help',
+        }
+      ]
+    }
+  },
+  methods: {
+    validationCheck() {
+      if (this.email === '') {
+        this.email_error = 'メールアドレスを入力してください';
+      } else if (!this.email.match(/^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/)) {
+        this.email_error = 'メールアドレスの形式が正しくありません';
+      } else {
+        document.gf_form.submit();
+        this.submitted = true;
+        this.email_error = '';
+      }
+    },
+  },
+  // Googleフォームのサンキューページに遷移しないようにする
+  mounted: function() {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute('name','hidden_iframe');
+    iframe.setAttribute('style','display: none');
+    document.body.appendChild(iframe);
+  }
+}
 </script>
 
 <style scoped>
@@ -56,15 +106,26 @@
 .input-email {
   width: 44.8rem;
   padding: 1.6rem 3.2rem;
+  border: 2px solid var(--orange3);
   border-radius:  1.2rem;
   color: var(--orange3);
   background: var(--orange4);
+}
+.input-email:focus {
+  color: var(--orange3);
+  background: var(--white);
+}
+.input-error {
+  border: 2px solid var(--red);
 }
 .btn-submit {
   margin-left: 1.6rem;
   background: var(--orange1);
   color: var(--white);
   border-radius:  1.2rem;
+}
+.error-msg {
+  color: var(--red);
 }
 
 </style>
